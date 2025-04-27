9dbf2ae3d7c2cbeb9ce2f25dd92c293c4cdadad2
geeta <geetatestingpurpose@gamil.com>
2025-04-27 17:11:46 +0530

    developer code one

--- src/Geestuff/geeGit.java
+++ src/Geestuff/geeGit.java
@@ -5,13 +5,15 @@ public class geeGit {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
+		
 		System.out.println("first person");
 		System.out.println("first person and first edit");
 		System.out.println("second person and first edit");
 		System.out.println("second person and second edit");
 		System.out.println("second person and third edit");
 		
-		
+		String x ="hello";
+		String x1 ="welcome";
 	}
 
 }
